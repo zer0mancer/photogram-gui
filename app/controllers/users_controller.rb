c9d54313f0649ab25e1b5bd6adfcde863a7d3102
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2025-01-08 06:08:46 +0000

    added users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({:username => :asc })
+    render({:template => "user_templates/index"})
+  end
+
+  def details
+input_username = params.fetch("path_id")
+
+matching_username = User.where({:username => input_username})
+
+@the_user = matching_username.at(0)
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+    render({:template => "user_templates/details"})
+    end
+
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 
+
+  get("/users", {:controller => "users", :action => "index"})
+  get("/users/:path_id", {:controller => "users", :action => "details"})
+
+
 end
